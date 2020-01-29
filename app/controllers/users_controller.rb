fc19c9c253dd238115b332d4e9133bfa34858fb7
huydaodang2706 <huydaodang2706@gmail.com>
2020-01-29 21:46:29 +0700

    Add user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,21 @@
 class UsersController <ApplicationController
   def new
-
+    @user = User.new
   end
-  
-  def show
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:success] = "New user has been created"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username,:password)
   end
 end
